17cf24274d6c55ec6bf820be35fcdd546eabf082
Nace Logar <the.code.destroyer@gmail.com>
2017-06-26 21:18:45 +0200

    Fix a bug with static method

--- src/helpers/MailerWrapper.js
+++ src/helpers/MailerWrapper.js
@@ -43,23 +43,23 @@ export class MailerWrapper {
         let htmlBody = `<h1>${this.mailTitle}</h1><br>`;
 
         if (!_.isEmpty(pushInfo.removedFilesArray)) {
-            htmlBody += MailerWrapper.createHtmlList('Removed Files', pushInfo.removedFilesArray, pushInfo.repoName, pushInfo.branchName);
+            htmlBody += this.createHtmlList('Removed Files', pushInfo.removedFilesArray, pushInfo.repoName, pushInfo.branchName);
         }
         if (!_.isEmpty(pushInfo.addedFilesArray)) {
-            htmlBody += MailerWrapper.createHtmlList('Added Files', pushInfo.addedFilesArray, pushInfo.repoName, pushInfo.branchName);
+            htmlBody += this.createHtmlList('Added Files', pushInfo.addedFilesArray, pushInfo.repoName, pushInfo.branchName);
         }
         if (!_.isEmpty(pushInfo.modifiedFilesArray)) {
-            htmlBody += MailerWrapper.createHtmlList('Modified Files', pushInfo.modifiedFilesArray, pushInfo.repoName, pushInfo.branchName);
+            htmlBody += this.createHtmlList('Modified Files', pushInfo.modifiedFilesArray, pushInfo.repoName, pushInfo.branchName);
         }
 
         return htmlBody;
     }
 
-    static createHtmlList(title, collection, repoName, branchName) {
+    createHtmlList(title, collection, repoName, branchName) {
         let html = `<h3>${title}:</h3><br>`;
         html += '<ul>';
         _.forEach(collection, (filePath) => {
-            if (PushInfoChecker.renderFileCheck(this.pathList, filePath)) {
+            if (!PushInfoChecker.renderFileCheck(this.pathList, filePath)) {
                 return;
             }
             const anchor = `<a target="_blank" href="https://github.com/${repoName}/blob/${branchName}/${filePath}">${filePath}</a>`;
